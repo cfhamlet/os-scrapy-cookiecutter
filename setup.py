8880252e808fce94d2faa429e8670bb26d6814df
Ozzy <cfhamlet@gmail.com>
2020-04-24 12:30:51 +0800

    fix: tox bug

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
